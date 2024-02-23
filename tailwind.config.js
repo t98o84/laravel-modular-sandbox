ad9dfd3cc4fcaa47f5b24e5ae41969750d8544b7
takuya829 <takuyaohashi829@gmail.com>
2024-02-23 17:22:33 +0900

    add @tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -9,5 +11,5 @@ export default {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [typography],
 }
